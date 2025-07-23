f71c81aa7bc0db49f1a546b94dfdfc191553935b
Valentin Jimenez De Santiago <santi.job.2022@gmail.com>
2025-07-23 00:10:48 -0600

    Initial browsing testing with browser_use

---
+++ Testing/Browser-TestAutomationFramework/README.md
@@ -0,0 +1,3 @@
+## Testing shopping using browser-use library.
+
+It's worth noticing that Claude accomplished the task on the first attempt All other models failed many times.
---
+++ Testing/Browser-TestAutomationFramework/src/Agent.py
@@ -0,0 +1,42 @@
+from browser_use.agent.service import Agent
+# from browser_use.agent.task import task
+from browser_use.llm import ChatGoogle, ChatOllama,ChatOpenAI, ChatDeepSeek, ChatAnthropic
+import asyncio
+
+from dotenv import load_dotenv
+load_dotenv()
+
+async def SiteValidation():
+    # Initialize the model
+    # llm = ChatGoogle(model='gemini-2.0-flash-exp')
+    # llm = ChatOllama(model="llama3.1:8b")   
+    # llm = ChatOpenAI(model="gpt-4o-mini")    
+    # llm = ChatDeepSeek(model="deepseek-chat")
+    llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")    
+
+    task = (
+        'Important : I am UI Automation tester validating the tasks'
+        'Open website https://rahulshettyacademy.com/loginpagePractise/'      
+        'If you need to visit google, ensure to click only on a page with the url: https://rahulshettyacademy.com/loginpagePractise/ do not try to access any other website'  
+        'Login with username and password. login Details available in the same page'
+        'After login, select first 2 products and add them to cart.'
+        'The 2 products you need to select are defined within an app-card and shoul have the opttion Add.'
+        'Then checkout and store the total value you see in screen'
+        'Increase the quantity of any product and check if total value update accordingly'
+        'checkout and select country, agree terms and purchase '
+        'verify thankyou message is displayed'
+    )
+
+    agent = Agent(
+        task=task,
+        llm=llm,
+        use_vision=False
+    )
+
+    history = await agent.run()
+    test_restult = history.final_result()
+    print(test_restult)
+
+
+asyncio.run(SiteValidation())
+    
