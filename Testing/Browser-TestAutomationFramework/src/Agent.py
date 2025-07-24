36db52525ff341576e9cbf57039001c0f4aaee89
Valentin Jimenez De Santiago <santi.job.2022@gmail.com>
2025-07-24 17:39:58 -0600

    Structured output using pydantic

--- Testing/Browser-TestAutomationFramework/src/Agent.py
+++ Testing/Browser-TestAutomationFramework/src/Agent.py
@@ -1,10 +1,21 @@
-from browser_use.agent.service import Agent
+from browser_use.agent.service import Agent, Controller
 # from browser_use.agent.task import task
 from browser_use.llm import ChatGoogle, ChatOllama,ChatOpenAI, ChatDeepSeek, ChatAnthropic
 import asyncio
+from pydantic import BaseModel
 
 from dotenv import load_dotenv
 load_dotenv()
+
+class CheckoutResult(BaseModel):
+    login_status: str
+    cart_status: str
+    checkout_status: str
+    total_update_status: str
+    delivery_location_status: str
+    confirmation_message: str
+
+controller = Controller(output_model = CheckoutResult)
 
 async def SiteValidation():
     # Initialize the model
@@ -30,13 +41,13 @@ async def SiteValidation():
     agent = Agent(
         task=task,
         llm=llm,
-        use_vision=False
+        use_vision=False,
+        controller=controller,
     )
 
     history = await agent.run()
     test_restult = history.final_result()
     print(test_restult)
-
 
 asyncio.run(SiteValidation())
     
