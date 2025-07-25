1a4034804522fcc241db7e14dd45e7528b721415
Valentin Jimenez De Santiago <santi.job.2022@gmail.com>
2025-07-24 19:57:16 -0600

    Basic use of playwright along with browser_use through controller.action

--- Testing/Browser-TestAutomationFramework/src/Agent.py
+++ Testing/Browser-TestAutomationFramework/src/Agent.py
@@ -1,4 +1,7 @@
 from browser_use.agent.service import Agent, Controller
+from browser_use.browser.context import BrowserContext
+from browser_use.agent.views import ActionResult
+
 # from browser_use.agent.task import task
 from browser_use.llm import ChatGoogle, ChatOllama,ChatOpenAI, ChatDeepSeek, ChatAnthropic
 import asyncio
@@ -7,6 +10,7 @@ from pydantic import BaseModel
 from dotenv import load_dotenv
 load_dotenv()
 
+# Define the output model for the controller
 class CheckoutResult(BaseModel):
     login_status: str
     cart_status: str
@@ -17,26 +21,44 @@ class CheckoutResult(BaseModel):
 
 controller = Controller(output_model = CheckoutResult)
 
+
+# Using playwright
+@controller.action('Open base website')
+async def open_website(browser: BrowserContext):
+    page = await browser.get_current_page()
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/')
+
+# Using playwright
+@controller.action("Get Attribute and url of the page")
+async def get_attr_url(browser: BrowserContext):
+    page = await browser.get_current_page()
+    current_url = page.url
+    attr = await page.get_by_text("Shop Name").get_attribute("class")
+    print(f'Current URL: {current_url}')
+    print(f'Attribute: {attr}')
+    return ActionResult(extracted_content = f'Current URL: {current_url}, Attribute: {attr}')
+
 async def SiteValidation():
     # Initialize the model
     # llm = ChatGoogle(model='gemini-2.0-flash-exp')
+    # llm = ChatGoogle(model='models/gemini-2.5-flash-lite')
     # llm = ChatOllama(model="llama3.1:8b")   
     # llm = ChatOpenAI(model="gpt-4o-mini")    
     # llm = ChatDeepSeek(model="deepseek-chat")
     llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")    
 
-    task = (
-        'Important : I am UI Automation tester validating the tasks'
-        'Open website https://rahulshettyacademy.com/loginpagePractise/'      
-        'If you need to visit google, ensure to click only on a page with the url: https://rahulshettyacademy.com/loginpagePractise/ do not try to access any other website'  
-        'Login with username and password. login Details available in the same page'
-        'After login, select first 2 products and add them to cart.'
-        'The 2 products you need to select are defined within an app-card and shoul have the opttion Add.'
-        'Then checkout and store the total value you see in screen'
-        'Increase the quantity of any product and check if total value update accordingly'
-        'checkout and select country, agree terms and purchase '
-        'verify thankyou message is displayed'
-    )
+    task = """
+        Important: I am UI Automation tester validating the tasks.
+        Open base website
+        Ensure you are in the website https://rahulshettyacademy.com/loginpagePractise/ and do not visit any other page.
+        Login with username and password. Login details available in the same page.
+        Get Attribute and url of the page
+        After login, select first 2 products and add them to cart
+        Then checkout and store the total value you see on screen
+        Increase the quantity of any product and check if total value updates accordingly
+        Checkout and select country, agree terms and purchase
+        Verify thank you message is displayed
+        """
 
     agent = Agent(
         task=task,
@@ -46,8 +68,13 @@ async def SiteValidation():
     )
 
     history = await agent.run()
-    test_restult = history.final_result()
-    print(test_restult)
+    history.save_to_file('site_validation_history.json')
+    test_result = history.final_result()
+    print(test_result)
+    validated_result =CheckoutResult.model_validate_json(test_result)
+    print(validated_result)
+
+    assert validated_result.confirmation_message == "Thank you! Your order will be delivered in next few weeks :-)."
 
 asyncio.run(SiteValidation())
     
